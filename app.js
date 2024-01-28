8a36e62c0db8fc72c082b061423288e5db441808
Juan17Informatico <juancampuzano2356@gmail.com>
2024-01-27 19:03:13 -0500

    Mejorar el diseño - Agregar diseño a al barra de menú

--- app.js
+++ app.js
@@ -1,56 +1,62 @@
+const miModulo = (() => {
+
+    const colorRgb1 = document.getElementById('colorRgb1');
+    const btnClick = document.getElementById('btn-click-1');
+    const section1 = document.getElementById('section1');
 
 
-const colorRgb1 = document.getElementById('colorRgb1');
-const btnClick1 = document.getElementById('btn-click1');
-const section1 = document.getElementById('section1');
-
-const randomNum = () =>{
-    return Math.floor(Math.random()*256);
-};
-
-const changeColor = () => {
-    const rgb = {
-        r: randomNum(), 
-        g: randomNum(), 
-        b: randomNum(),
+    const randomNum = () => {
+        return Math.floor(Math.random() * 256);
     };
 
-    return rgb;
-    
-    // document.body.style.backgroundColor = randomColor;
-    // colorRgb1.innerText = randomColor;
-}
+    const changeColor = () => {
+        const rgb = {
+            r: randomNum(),
+            g: randomNum(),
+            b: randomNum(),
+        };
 
-const asignarColor = (r , g , b, id) => {
-    
-    let colorStyle = `rgb(${r},${g},${b})`;
+        return rgb;
 
-    console.log(colorStyle)
-    id.style.backgroundColor = colorStyle;
-}
-
-function rgbColor(rgb){
-    console.log({rgb})
-    const keys = Object.keys(rgb);
-    const values = Object.values(rgb);
-    console.log(keys, values);
-    const rgbNewValue = []
-    for (let i = 0; i < keys.length; i++) {
-        rgbNewValue.push(keys.i);
-        console.log(rgbNewValue);
+        // document.body.style.backgroundColor = randomColor;
+        // colorRgb1.innerText = randomColor;
     }
-}
 
-function rgbToHex(rgb){
+    const asignarColor = (r, g, b, id) => {
 
-}
+        let colorStyle = `rgb(${r},${g},${b})`;
 
-btnClick1.addEventListener("click", () => {
-    let rgb = changeColor();
+        console.log(colorStyle)
+        id.style.backgroundColor = colorStyle;
+    }
 
-    asignarColor(rgb.r, rgb.g, rgb.b, section1);
-    
-});
-window.addEventListener("load", changeColor);
+    function rgbColor(rgb) {
+        console.log({ rgb })
+        const keys = Object.keys(rgb);
+        const values = Object.values(rgb);
+        console.log(keys, values);
+        const rgbNewValue = []
+        for (let i = 0; i < keys.length; i++) {
+            rgbNewValue.push(keys.i);
+            console.log(rgbNewValue);
+        }
+    }
 
+    function rgbToHex(rgb) {
+
+    }
+
+
+
+    btnClick.addEventListener("click", () => {
+        let rgb = changeColor();
+
+        asignarColor(rgb.r, rgb.g, rgb.b, section1);
+
+    });
+    window.addEventListener("load", changeColor);
+
+
+
+})();
 
