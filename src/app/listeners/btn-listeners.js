a5e8ae9f393814abc54d8cee517cbf06df315361
Juan17Informatico <juancampuzano2356@gmail.com>
2024-03-19 06:14:09 -0500

    Se configuro correctamente la accion de random y hacer que se muestren los colores en texto en rgb y hexadecimal

--- src/app/listeners/btn-listeners.js
+++ src/app/listeners/btn-listeners.js
@@ -5,53 +5,23 @@ import { changeColor, asignarColor, rgbColor, rgbToText, rgbToHex } from "./../t
  * @param {HTMLElementDiv} btn 
  * @param {Array|IdSections} sections 
  */
-// const randomColor = (btn, sections) => {
-//     btn.addEventListener('click', () => {
-//         let rgbs = {
-//             rgb1: changeColor(),
-//             rgb2: changeColor(),
-//             rgb3: changeColor(),
-//             rgb4: changeColor(),
-//         };
-//         const { rgb1,
-//             rgb2,
-//             rgb3,
-//             rgb4, } = rgbs;
+export const randomColor = (section, resultHex, resultRgb) => {
 
-//         for (let i = 0; i < sections.length; i++) {
+    let rgb = changeColor();
+    const sectionDiv = document.getElementById(section);
+    asignarColor(rgb.r, rgb.g, rgb.b, sectionDiv);
 
-//             asignarColor(rgb1.r, rgb1.g, rgb4.b, section[i]);
+    let hex = rgbToHex(rgb);
+    rgbToText(hex, resultHex);
 
-//         }
+    let rgbText = rgbColor(rgb);
+    rgbToText(rgbText, resultRgb);
 
-//         asignarColor(rgb2.r, rgb2.g, rgb4.b, section2);
-//         asignarColor(rgb3.r, rgb3.g, rgb4.b, section3);
-//         asignarColor(rgb4.r, rgb4.g, rgb4.b, section4);
 
-//         let rgbText1 = rgbColor(rgb1);
-//         let rgbText2 = rgbColor(rgb2);
-//         let rgbText3 = rgbColor(rgb3);
-//         let rgbText4 = rgbColor(rgb4);
 
-//         rgbToText(rgbText1, resultRgb1);
-//         rgbToText(rgbText2, resultRgb2);
-//         rgbToText(rgbText3, resultRgb3);
-//         rgbToText(rgbText4, resultRgb4);
+}
 
-//         let hex1 = rgbToHex(rgb1);
-//         let hex2 = rgbToHex(rgb2);
-//         let hex3 = rgbToHex(rgb3);
-//         let hex4 = rgbToHex(rgb4);
-
-//         rgbToText(hex1, resultHexa1);
-//         rgbToText(hex2, resultHexa2);
-//         rgbToText(hex3, resultHexa3);
-//         rgbToText(hex4, resultHexa4);
-
-//     });
-// }
-
-export const cambiarColor = (section) => {
+export const cambiarColor = (section, resultHex, resultRgb) => {
 
     const sectionDiv = document.getElementById(section);
 
@@ -59,10 +29,10 @@ export const cambiarColor = (section) => {
 
     asignarColor(rgb.r, rgb.g, rgb.b, sectionDiv);
 
-    // let rgbText = rgbColor(rgb);
+    let rgbText = rgbColor(rgb);
 
-    // rgbToText(rgbText, resultRgb1);
-    // let hex = rgbToHex(rgb);
-    // rgbToText(hex, resultHexa1);
+    rgbToText(rgbText, resultRgb);
+    let hex = rgbToHex(rgb);
+    rgbToText(hex, resultHex);
 
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { renderContent } from "./app/renderHTML/render.js";
-import {cambiarColor}  from "./app/listeners/btn-listeners.js";
+import {cambiarColor, randomColor}  from "./app/listeners/btn-listeners.js";
 
 
 export const initApp = (element) => {
@@ -8,20 +8,36 @@ export const initApp = (element) => {
     const btnClick2 = document.getElementById('btnClick2');
     const btnClick3 = document.getElementById('btnClick3');
     const btnClick4 = document.getElementById('btnClick4');
+    const random = document.getElementById('random');
+    const resultHexa1 = document.querySelector("#result-hexa-span-1");
+    const resultHexa2 = document.querySelector("#result-hexa-span-2");
+    const resultHexa3 = document.querySelector("#result-hexa-span-3");
+    const resultHexa4 = document.querySelector("#result-hexa-span-4");
+    const resultRgb1 = document.querySelector("#result-rgb-span-1");
+    const resultRgb2 = document.querySelector("#result-rgb-span-2");
+    const resultRgb3 = document.querySelector("#result-rgb-span-3");
+    const resultRgb4 = document.querySelector("#result-rgb-span-4");
+
+    random.addEventListener('click', () => {
+        randomColor('section1', resultHexa1, resultRgb1);
+        randomColor('section2', resultHexa2, resultRgb2);
+        randomColor('section3', resultHexa3, resultRgb3);
+        randomColor('section4', resultHexa4, resultRgb4);
+    });
 
     btnClick1.addEventListener('click', ()=>{
-        cambiarColor("section1");
+        cambiarColor("section1", resultHexa1, resultRgb1);
     }); 
 
     btnClick2.addEventListener('click', ()=>{
-        cambiarColor("section2");
+        cambiarColor("section2", resultHexa2, resultRgb2);
     }); 
 
     btnClick3.addEventListener('click', ()=>{
-        cambiarColor("section3");
+        cambiarColor("section3", resultHexa3, resultRgb3);
     }); 
 
     btnClick4.addEventListener('click', ()=>{
-        cambiarColor("section4");
+        cambiarColor("section4", resultHexa4, resultRgb4);
     }); 
 }
